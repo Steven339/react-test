4c81d3ce4027771d33bea9b6ebe2e3719e3ad3c3
Brayan Correa <brayancorrea78@gmail.com>
2024-03-05 12:03:42 -0500

    fixes

--- src/pages/products/components/AddProducts.js
+++ src/pages/products/components/AddProducts.js
@@ -1,21 +1,29 @@
 import { Box, Button, Modal, TextField } from "@mui/material";
 import { useState } from "react";
-import {addProducts} from "../services/Porducts.service";
+import { addProducts } from "../services/Porducts.service";
 
 export const AddProducts = () => {
 	const [open, setOpen] = useState(false);
+	const [name, setName] = useState("");
+	const [price, setPrice] = useState("");
+	const [priceWithDiscount, setPriceWithDiscount] = useState("");
+	const [discountType, setDiscountType] = useState("");
+	const [stock, setStock] = useState("");
+	
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
-
+	
 	const handleSave = () => {
 		const product = {
-			name: "test",
-			price: 10,
-			priceWithDiscount: 10,
-			discountType: "percentage",
-			stock: 10
+			name: name,
+			price: parseInt(price),
+			priceWithDiscount: parseInt(priceWithDiscount),
+			discountType: discountType,
+			stock: parseInt(stock)
 		}
+		console.log(product);
 		addProducts(product);
+		handleClose();
 	}
 	return (
 		<div>
@@ -28,11 +36,16 @@ export const AddProducts = () => {
 			>
 				<Box className={"modal"}>
 					<h2>Crear producto</h2>
-					<TextField id="outlined-basic" label="Nombre" variant="outlined" />
-					<TextField id="outlined-basic" label="Precio" variant="outlined" />
-					<TextField id="outlined-basic" label="Precio con descuento" variant="outlined" />
-					<TextField id="outlined-basic" label="Tipo de descuento" variant="outlined" />
-					<TextField id="outlined-basic" label="Cantidad" variant="outlined" />
+					<TextField id="outlined-basic" label="Nombre" variant="outlined" value={name}
+					           onChange={(e) => setName(e.target.value)} />
+					<TextField id="outlined-basic" label="Precio" variant="outlined" value={price}
+					           onChange={(e) => setPrice(e.target.value)} />
+					<TextField id="outlined-basic" label="Precio con descuento" variant="outlined" value={priceWithDiscount}
+					           onChange={(e) => setPriceWithDiscount(e.target.value)} />
+					<TextField id="outlined-basic" label="Tipo de descuento" variant="outlined" value={discountType}
+					           onChange={(e) => setDiscountType(e.target.value)} />
+					<TextField id="outlined-basic" label="Cantidad" variant="outlined" value={stock}
+					           onChange={(e) => setStock(e.target.value)} />
 					<Button variant="text" onClick={handleClose}>Cancelar</Button>
 					<Button variant="contained" onClick={handleSave}>Guardar</Button>
 				</Box>
